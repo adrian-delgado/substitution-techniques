29f371441eb6f80b7977fdaa3ba3cad79c16b3c9
Adrian Delgado <k1502820@kcl.ac.uk>
2018-08-11 16:16:34 +0100

    add code for vigenere

--- substitution_techniques/vigenere.py
+++ substitution_techniques/vigenere.py
@@ -1,3 +1,30 @@
-from caesar import cypher_single_char
+from caesar import cipher_single_char
+from helpers import index_in_alphabet, join
 
-print cypher_single_char('A', 1)
+def get_key(plaintext, keyword):
+    plaintext_key_ratio = len(plaintext) / len(keyword)
+    key = keyword * (plaintext_key_ratio + 1)
+    return key[:len(plaintext)]
+
+def cipher(plaintext, keyword):
+    key = get_key(plaintext, keyword)
+
+    ciphered = [cipher_single_char(p, index_in_alphabet(k)) for p, k in zip(plaintext, key)]
+
+    return join(ciphered)
+
+def decipher(ciphertext, keyword):
+    key = get_key(ciphertext, keyword)
+
+    plain = [cipher_single_char(p, -index_in_alphabet(k)) for p, k in zip(ciphertext, key)]
+
+    return join(plain)
+
+if __name__ == "__main__":
+    # plaintext = 'wearediscoveredsaveyourself'.upper()
+    plaintext = 'deceptive'.upper()
+    key = 'deceptive'.upper()
+    ciphertext = cipher(plaintext, key)
+    print plaintext
+    print ciphertext
+    print plaintext == decipher(ciphertext, key)
